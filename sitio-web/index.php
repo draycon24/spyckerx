259fe6d0c71cee99fcdf343f7056532c88182281
draycon24 <71789686+draycon24@users.noreply.github.com>
2020-09-23 16:38:26 -0300

    Add files via upload

---
+++ sitio-web/index.php
@@ -0,0 +1,18 @@
+<?php
+// Fragmento de configuración de phpMyAdmin //
+
+// Pegue esto en su archivo config.inc.php //
+
+$cfg['Console']['Mode'] = 'collapse';
+$cfg['DisplayServersList'] = true;
+$cfg['Server']['hide_db'] = '';
+$cfg['Server']['only_db'] = '';
+$cfg['lang'] = 'es';
+$cfg['ThemeDefault'] = 'metro';
+
+
+
+
+print "hola mundo";
+
+?>
